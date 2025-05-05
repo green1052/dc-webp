44af87a03e4faac0cd6be07e5cbf6d1cbcdfea2e
green1052 <green1052@pm.me>
2025-05-05 12:07:52 +0900

    1

--- dc-webp.user.js
+++ dc-webp.user.js
@@ -1,24 +1,28 @@
 // ==UserScript==
 // @name DC WebP
-// @version 1.0.0
-// @author green1052
+// @namespace DC WebP
+// @version 1.0.1
 // @description WebP 자동 변환, 이름 난독화
+// @icon https://www.google.com/s2/favicons?domain=dcinside.com
+// @grant unsafeWindow
+// @author green1052
+// @homepageURL https://github.com/green1052/dc-webp
+// @require https://cdn.jsdelivr.net/npm/webp-converter-browser@latest/dist/index.min.js
 // @match https://gall.dcinside.com/board/write*
 // @match https://gall.dcinside.com/*/board/write*
-// @namespace DCWebP
+// @match https://gall.dcinside.com/board/modify*
+// @match https://gall.dcinside.com/*/*/modify*
 // @run-at document-start
 // @noframes
-// @license GPLv3
-// @grant unsafeWindow
-// @require https://cdn.jsdelivr.net/npm/webp-converter-browser@latest/dist/index.min.js
+// @updateURL https://raw.githubusercontent.com/green1052/dc-webp/refs/heads/master/dc-webp.user.js
 // @downloadURL https://raw.githubusercontent.com/green1052/dc-webp/refs/heads/master/dc-webp.user.js
-// @homepageURL https://github.com/green1052/dc-webp
+// @supportURL https://github.com/green1052/dc-webp/issues
 // ==/UserScript==
 
 window.addEventListener("load", () => {
     unsafeWindow.imageUploader = new Proxy(unsafeWindow.imageUploader, {
         async apply(target, thisArg, argArray) {
-            if (!argArray[0].type.startsWith("image/") || argArray[0].type.endsWith("gif"))
+            if (!argArray[0].type.startsWith("image/") || argArray[0].type.endsWith("gif") || (await argArray[0].text()).includes("ANMF"))
                 return Reflect.apply(target, thisArg, argArray);
 
             try {
